bd739770a6538743c48ee16e20751b131f3d182a
Will <wb.premiertech@gmail.com>
2022-09-10 10:48:25 -0300

    iniciando projeto

---
+++ src/main/java/br/com/pit2/wb/Pit2WbApplication.java
@@ -0,0 +1,13 @@
+package br.com.pit2.wb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pit2WbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pit2WbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pit2/wb/Pit2WbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pit2.wb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pit2WbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
